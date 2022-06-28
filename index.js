47c07f793c6d11dfe2b0af26c78fb4abaa4039f1
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-06-28 19:07:11 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+class Polygon {
+    constructor(sides) {
+        this.sides = sides;
+    }
+    get countSides() {
+        return this.sides.length;
+    }
+    get perimeter() {
+        return this.sides.reduce((a, b) => a + b);
+    }
+    }
+
+    class Triangle extends Polygon {
+        constructor(sides) {
+            super(sides);
+        }
+        get isValid() {
+            return this.sides[0] + this.sides[1] > this.sides[2] &&
+                this.sides[1] + this.sides[2] > this.sides[0] &&
+                this.sides[2] + this.sides[0] > this.sides[1];
+        }
+    }
+
+    class Square extends Polygon {
+        constructor(sides) {
+            super(sides);
+        }
+        get isValid() {
+            return this.sides[0] === this.sides[1] &&
+                this.sides[1] === this.sides[2] &&
+                this.sides[2] === this.sides[3];
+        }
+        get area(){
+            return this.sides[0] * this.sides[0];
+        }
+    }
